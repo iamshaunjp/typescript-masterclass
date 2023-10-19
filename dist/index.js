d95f4a6aff4b7eb97b9934ca30522d7fbaf4a139
Shaun <shaun@thenetninja.co.uk>
2023-10-19 14:28:48 +0100

    lesson-4

---
+++ dist/index.js
@@ -0,0 +1,34 @@
+"use strict";
+//------------------------
+// basic type annotations
+//------------------------
+let age = 30;
+let firstName = 'Mario';
+let isFictional;
+// age = 'hello'
+age = 31;
+// firstName = 100
+firstName = 'Luigi';
+// isFictional = 'false'
+isFictional = false;
+//----------------
+// type inference
+//----------------
+let planet = 'Earth';
+let moons = 1;
+let isLarge = false;
+// planet = 10
+planet = 'Saturn';
+// moons = '145'
+moons = 145;
+// isLarge = 'yes'
+isLarge = true;
+//------------------
+// null & undefined
+//------------------
+let something;
+let anotherThing;
+// something = 10
+something = null;
+// anotherThing = 'hello, world'
+anotherThing = undefined;
