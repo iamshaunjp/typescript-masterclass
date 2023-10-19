2d65cf27a666ce17db54560f52817d95316c2879
Shaun <shaun@thenetninja.co.uk>
2023-10-19 14:21:25 +0100

    lesson-3

--- index.ts
+++ index.ts
@@ -1,4 +1,46 @@
-const firstName = 'mario'
-const age = 30
+//------------------------
+// basic type annotations
+//------------------------
 
-console.log(firstName, age)
+let age: number = 30
+let firstName: string = 'Mario'
+let isFictional: boolean
+
+// age = 'hello'
+age = 31
+
+// firstName = 100
+firstName = 'Luigi'
+
+// isFictional = 'false'
+isFictional = false
+
+//----------------
+// type inference
+//----------------
+
+let planet = 'Earth'
+let moons = 1
+let isLarge = false
+
+// planet = 10
+planet = 'Saturn'
+
+// moons = '145'
+moons = 145
+
+// isLarge = 'yes'
+isLarge = true
+
+//------------------
+// null & undefined
+//------------------
+
+let something: null
+let anotherThing: undefined
+
+// something = 10
+something = null
+
+// anotherThing = 'hello, world'
+anotherThing = undefined
