fd8486d368cfc2dd114f3d133016981be5e1a4f4
Shaun <shaun@thenetninja.co.uk>
2023-10-28 10:00:47 +0100

    lesson-37

--- src/ts/services/DataResource.ts
+++ src/ts/services/DataResource.ts
@@ -6,7 +6,25 @@ export class DataResource<T> {
 
 		return res.json()
 	}
-	async loadOne(id: number) {}
-	async delete(id: number) {}
-	async save(data: T) {}
+	async loadOne(id: number): Promise<T> {
+		const res = await fetch(`${this.endpoint}/${id}`)
+
+		return res.json()
+	}
+	async delete(id: number): Promise<Response> {
+		const res = await fetch(`${this.endpoint}/${id}`, {
+			method: 'DELETE',
+		})
+
+		return res
+	}
+	async save(data: T): Promise<Response> {
+		const res = await fetch(this.endpoint, {
+			method: 'POST',
+			body: JSON.stringify(data),
+			headers: { 'Content-Type': 'application/json' },
+		})
+
+		return res
+	}
 }
