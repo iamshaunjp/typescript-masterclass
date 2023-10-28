e5e7a65063b68bfb939e25aa3e79745e46143d52
Shaun <shaun@thenetninja.co.uk>
2023-10-28 14:20:27 +0100

    lesson-40

--- src/ts/create.ts
+++ src/ts/create.ts
@@ -1,12 +1,25 @@
-const titleInput = document.querySelector(
-	'input[name="title"]'
-) as HTMLInputElement
-const descriptionInput = document.querySelector(
-	'textarea'
-) as HTMLTextAreaElement
+import { Pizza, PizzaProps } from './models/Pizza'
 
 const form = document.querySelector('.create') as HTMLFormElement
 
 form.addEventListener('submit', async (e) => {
 	e.preventDefault()
+
+	const data = new FormData(form)
+
+	const newPizza: PizzaProps = {
+		title: data.get('title') as string,
+		description: data.get('description') as string,
+		toppings: data.getAll('toppings') as string[],
+		price: parseInt(data.get('price') as string),
+	}
+
+	const res = await Pizza.save(newPizza)
+
+	if (!res.ok) {
+		console.log('not able to save the pizza')
+	}
+	if (res.ok) {
+		window.location.href = '/'
+	}
 })
