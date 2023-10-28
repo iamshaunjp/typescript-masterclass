04bcf783af349434257f80b8b6b43512344dc75e
Shaun <shaun@thenetninja.co.uk>
2023-10-28 13:33:36 +0100

    lesson-38

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,35 @@
-import { Pizza } from './models/Pizza'
+import { Pizza, PizzaProps } from './models/Pizza'
+
+const rootElement = document.querySelector('.root')!
+
+function createPizzaTemplate(pizza: PizzaProps): string {
+	return `
+    <div class="pizza">
+      <h2>${pizza.title}...</h2>
+      <p class="toppings">${pizza.toppings.join(', ')}</p>
+      <p>${pizza.description}</p>
+      <span>£${pizza.price}</span>
+    </div>
+  `
+}
+
+function renderTemplates(templates: string[], parent: Element): void {
+	const templateElement = document.createElement('template')
+
+	for (const t of templates) {
+		templateElement.innerHTML += t
+	}
+
+	parent.append(templateElement.content)
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
-	// load the pizzas data
+	// load the pizza data
 	const pizzas = await Pizza.loadAll()
 
-	console.log(pizzas)
+	// create template string for each pizza
+	const pizzaTemplates = pizzas.map(createPizzaTemplate)
+
+	// render pizza templates to DOM
+	renderTemplates(pizzaTemplates, rootElement)
 })
