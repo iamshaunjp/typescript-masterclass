02ef5ce44e25df5918eb4afce920a177c4aa9793
Shaun <shaun@thenetninja.co.uk>
2023-10-29 10:51:45 +0000

    lesson-41

---
+++ ninjablog/src/app/about/page.tsx
@@ -0,0 +1,28 @@
+export default function About() {
+	return (
+		<main>
+			<h2>About</h2>
+			<article>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic nostrum
+					praesentium sunt sint magnam est, reiciendis temporibus ipsum illo
+					suscipit debitis unde ex nemo quo officia harum molestias animi
+					possimus?
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
+					nesciunt. Illo unde animi sapiente veritatis officiis autem obcaecati
+					alias reiciendis? Nemo, qui? Vitae tempora blanditiis unde doloribus
+					iusto voluptas ad labore esse qui numquam, ratione aspernatur ut.
+					Exercitationem maxime atque dolore mollitia nemo tempore corrupti
+					veniam laudantium cupiditate aliquam architecto saepe voluptates, ea
+					adipisci obcaecati accusantium facere modi quibusdam fugit cum
+					incidunt, aperiam unde facilis. Culpa, eum! Unde quam perferendis
+					omnis, praesentium numquam possimus veritatis cumque ullam atque
+					delectus quis similique facilis dicta qui consequatur. Earum aut
+					mollitia libero consequatur?
+				</p>
+			</article>
+		</main>
+	)
+}
